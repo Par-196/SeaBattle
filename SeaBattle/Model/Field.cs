d1d47c089eb6c436ae701157704e0a50bbd27a06
Par-196 <153231354+Par-196@users.noreply.github.com>
2025-05-10 21:45:53 +0200

    Push for laptop

--- SeaBattle/Model/Field.cs
+++ SeaBattle/Model/Field.cs
@@ -1,6 +1,7 @@
 ﻿using SeaBattle.Model.Enum;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Security.Policy;
@@ -18,11 +19,12 @@ namespace SeaBattle.Model
         public Field() 
         {
             //todo: add boarders
-            Cells = new Cell[12, 12];
+            Cells = new Cell[13, 13];
             for (int i = 0; i < Cells.GetLength(0); i++)
             {
                 for (int j = 0; j < Cells.GetLength(0); j++)
                 {
+
                     if (i == 0 || i == 11)
                     {
                         Cells[i, j] = new Cell(TypeCell.Border);
@@ -43,7 +45,7 @@ namespace SeaBattle.Model
             {
                 for (int j = 0; j < Cells.GetLength(0); j++)
                 {
-                    Console.Write(Cells[i ,j]);
+                    Console.Write($" {Cells[i, j]}");
                 }
                 Console.WriteLine();
             }
@@ -54,88 +56,117 @@ namespace SeaBattle.Model
             TypeShips type = TypeShips.OneCellShip;
             ShipDirection direction = ShipDirection.Up;
             var point = new Point();
-            int oneDeckShips = 4;
-            int twoDeckShips = 3;
-            int threeDeckShips = 2;
-            int fourDeckShips = 1;
+            int oneCellShip = 4;
+            int twoCellShip = 3;
+            int threeCellShip = 2;
+            int fourCellShip = 1;
 
-            int allShips = oneDeckShips + twoDeckShips + threeDeckShips + fourDeckShips;
+            int allShips = oneCellShip + twoCellShip + threeCellShip + fourCellShip;
 
             while (allShips != 0)
             {
-                Console.WriteLine("Виберiть корабель" + "\n" +
-                    "1. Однопалубний" + "\n" +
-                    "2. Двопалубний" + "\n" +
-                    "3. Трипалубний" + "\n" +
-                    "4. Чотирьохпалубний");
-
-                if (oneDeckShips != 0 || twoDeckShips != 0 || threeDeckShips != 0 || fourDeckShips != 0)
+                do
                 {
-                    if (System.Enum.TryParse(Console.ReadLine(), out type))
+                    Console.WriteLine("Виберiть корабель" + "\n" +
+                        "1. Однопалубний" + "\n" +
+                        "2. Двопалубний" + "\n" +
+                        "3. Трипалубний" + "\n" +
+                        "4. Чотирьохпалубний");
+                    if (System.Enum.TryParse(Console.ReadLine(), out type));
+                } while (type != TypeShips.OneCellShip && type != TypeShips.TwoCellShip && type != TypeShips.ThreeCellShip && type != TypeShips.FourCellShip);
+                {
+                    switch (type)
                     {
-                        do
-                        {
-                            Console.WriteLine("Enter cord x");
-                            point.X = int.Parse(Console.ReadLine());
-
-                            Console.WriteLine("Enter cord y");
-                            point.Y = int.Parse(Console.ReadLine());
-
-                        } while (point.X < 1 || point.X > 11 || point.Y < 1 || point.Y > 11 ||
-                            Cells[point.X, point.Y].Value != TypeCell.Empty || AnyShipsAround(point));
-
-                        if (type != TypeShips.OneCellShip)
-                        {
-                            Console.WriteLine("Enter direction" + "\n" +
-                                "1. Вгору" + "\n" +
-                                "2. Вниз" + "\n" +
-                                "3. Вліво" + "\n" +
-                                "4. Вправо");
-                            System.Enum.TryParse(Console.ReadLine(), out direction);
-
-                            switch (type)
+                        case TypeShips.OneCellShip:
                             {
-                                case TypeShips.TwoCellShip:
-                                    {
-                                        SetShip(point, direction, type);
-                                        twoDeckShips--;
-                                        ShowField();
-                                    }
-                                    break;
-                                case TypeShips.ThreeCellShip:
-                                    {
-                                        SetShip(point, direction, type);
-                                        threeDeckShips--;
-                                        ShowField();
-                                    }
-                                    break;
-                                case TypeShips.FourCellShip:
-                                    {
-                                        SetShip(point, direction, type);
-                                        fourDeckShips--;
-                                        ShowField();
-                                    }
-                                    break;
-                                default:
-                                    break;
+                                if (oneCellShip == 0) 
+                                {
+                                    Console.WriteLine("Ви можете розмістити тільки 4 однопалубних корабля");
+                                    continue;
+                                }
                             }
-                        }
-                        else if (type == TypeShips.OneCellShip)
+                            break;
+                        case TypeShips.TwoCellShip:
+                            {
+                                if (twoCellShip == 0)
+                                {
+                                    Console.WriteLine("Ви можете розмістити тільки 3 двопалубних корабля");
+                                    continue;
+                                }
+                            }
+                            break;
+                        case TypeShips.ThreeCellShip:
+                            {
+                                if (threeCellShip == 0)
+                                {
+                                    Console.WriteLine("Ви можете розмістити тільки 2 триопалубних корабля");
+                                    continue;
+                                }
+                            } 
+                            break;
+                        case TypeShips.FourCellShip:
+                            {
+                                if (fourCellShip == 0)
+                                {
+                                    Console.WriteLine("Ви можете розмістити тільки 1 чотирипалубних корабля");
+                                    continue;
+                                }
+                            }
+                            break;
+                    }
+                    do
+                    {
+                        Console.WriteLine("Enter cord x");
+                        point.X = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Enter cord y");
+                        point.Y = int.Parse(Console.ReadLine());
+
+                    } while (point.X < 1 || point.X > 11 || point.Y < 1 || point.Y > 11 ||
+                        Cells[point.X, point.Y].Value != TypeCell.Empty || AnyShipsAroundPoint(point));
+
+                    if (type != TypeShips.OneCellShip)
+                    {
+                        Console.WriteLine("Enter direction" + "\n" +
+                            "1. Вгору" + "\n" +
+                            "2. Вниз" + "\n" +
+                            "3. Вліво" + "\n" +
+                            "4. Вправо");
+                        System.Enum.TryParse(Console.ReadLine(), out direction);
+
+                        switch (type)
                         {
-                            SetShip(point, direction, type);
-                            oneDeckShips--;
-                            ShowField();
+                            case TypeShips.TwoCellShip:
+                                {
+                                    SetShip(point, direction, type, oneCellShip, twoCellShip, threeCellShip, fourCellShip);
+                                    ShowField();
+                                }
+                                break;
+                            case TypeShips.ThreeCellShip:
+                                {
+                                    SetShip(point, direction, type, oneCellShip, twoCellShip, threeCellShip, fourCellShip);
+                                    ShowField();
+                                }
+                                break;
+                            case TypeShips.FourCellShip:
+                                {
+                                    SetShip(point, direction, type, oneCellShip, twoCellShip, threeCellShip, fourCellShip);
+                                    ShowField();
+                                }
+                                break;
+                            default:
+                                break;
                         }
-                        else
-                        {
-                            Console.WriteLine("Некоректний ввiд");
-                        }
+                    }
+                    else if (type == TypeShips.OneCellShip)
+                    {
+                        SetShip(point, direction, type, oneCellShip, twoCellShip, threeCellShip, fourCellShip);
+                        ShowField();
                     }
                     else
                     {
-                        Console.WriteLine("Ви не можете бiльше розмiстити кораблi даного типу");
+                        Console.WriteLine("Некоректний ввiд");
                     }
-
                 }
             }
         }
@@ -147,27 +178,178 @@ namespace SeaBattle.Model
         }
 
 
-        public void SetShip(Point point, ShipDirection direction, TypeShips type)
+        public void SetShip(Point point, ShipDirection direction, TypeShips type, int oneCellShip, int twoCellShip, int threeCellShip, int fourCellShip)
         {
             var ship = CreateShip(point.X, point.Y, direction, type);
 
-
+            if (ShipIsOutsideField(ship, point, direction))
+            {
+                Console.WriteLine("Корабель виходить за межi поля");
+                return;
+            }
+            if (!AnyShipAroundShip(point, ship, direction))
+            {
+                Console.WriteLine("Довкола є iншi кораблi, будь ласка виберiть iнше мiсце для вашого корабля");
+                return;
+            }
+            switch (type)
+            {
+                case TypeShips.OneCellShip:
+                    {
+                        oneCellShip--;
+                    }
+                    break;
+                case TypeShips.TwoCellShip:
+                    {
+                        twoCellShip--;
+                    }
+                    break;
+                case TypeShips.ThreeCellShip:
+                    {
+                        threeCellShip--;
+                    }
+                    break;
+                case TypeShips.FourCellShip:
+                    {
+                        fourCellShip--;
+                    }
+                    break;
+            }
             foreach (var item in ship.Points)
-            { 
+            {
                 for (int x = 0; x < Cells.GetLength(0); x++)
-                { 
+                {
                     for (int y = 0; y < Cells.GetLength(1); y++)
                     {
-                        if (x == item.X  && y == item.Y)
+                        if (x == item.X && y == item.Y)
                         {
                             Cells[x, y].Value = TypeCell.ShipBody;
                         }
                     }
                 }
             }
-
         }
 
+        public bool ShipIsOutsideField(Ship ship, Point point, ShipDirection direction)
+        {
+            switch (direction)
+            {
+                case ShipDirection.Up:
+                    {
+                        if (point.X < 1 || point.Y - ship.Points.Count < 0)
+                        {
+                            return true;
+                        }
+                    }
+                    break;
+                case ShipDirection.Down:
+                    {
+                        if (point.X > 12 || point.Y + ship.Points.Count > 11)
+                        {
+                            return true;
+                        }
+                    }
+                    break;
+                case ShipDirection.Left:
+                    {
+                        if (point.X < 1 || point.Y - ship.Points.Count < 0)
+                        {
+                            return true;
+                        }
+                    }
+                    break;
+                case ShipDirection.Right:
+                    {
+                        if (point.X > 12 || point.Y + ship.Points.Count > 11)
+                        {
+                            return true;
+                        }
+                    }
+                    break;
+            }
+            return false;
+        }
+
+
+
+        public bool AnyShipAroundShip(Point point, Ship ship, ShipDirection direction)
+        {
+            int countOperation = 0;
+            foreach (var item in ship.Points)
+            {
+                switch (direction)
+                {
+                    case ShipDirection.Up:
+                        {
+                            countOperation++;
+                            if (Cells[item.X - 1, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X - 1, item.Y].Value == TypeCell.ShipBody || Cells[item.X - 1, item.Y + 1].Value == TypeCell.ShipBody ||
+                            Cells[item.X, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X, item.Y + 1].Value == TypeCell.ShipBody ||
+                            Cells[item.X + 1, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X + 1, item.Y + 1].Value == TypeCell.ShipBody)
+                            {
+                                
+                                return false;
+                            }
+                            else
+                            {
+                                if (countOperation == ship.Points.Count)
+                                return true;
+                            }
+                        }
+                        break;
+                    case ShipDirection.Down:
+                        {
+                            countOperation++;
+                            if (Cells[item.X - 1, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X - 1, item.Y + 1].Value == TypeCell.ShipBody ||
+                            Cells[item.X, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X, item.Y + 1].Value == TypeCell.ShipBody ||
+                            Cells[item.X + 1, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X + 1, item.Y].Value == TypeCell.ShipBody || Cells[item.X + 1, item.Y + 1].Value == TypeCell.ShipBody)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                if (countOperation == ship.Points.Count)
+                                    return true;
+                            }
+                        }
+                        break;
+                    case ShipDirection.Left:
+                        {
+                            countOperation++;
+                            if (Cells[item.X - 1, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X - 1, item.Y].Value == TypeCell.ShipBody || Cells[item.X - 1, item.Y + 1].Value == TypeCell.ShipBody ||
+                            Cells[item.X, item.Y - 1].Value == TypeCell.ShipBody ||
+                            Cells[item.X + 1, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X + 1, item.Y].Value == TypeCell.ShipBody || Cells[item.X + 1, item.Y + 1].Value == TypeCell.ShipBody)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                if (countOperation == ship.Points.Count)
+                                    return true;
+                            }
+                        }
+                        break;
+                    case ShipDirection.Right:
+                        {
+                            countOperation++;
+                            if (Cells[item.X - 1, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X - 1, item.Y].Value == TypeCell.ShipBody || Cells[item.X - 1, item.Y + 1].Value == TypeCell.ShipBody ||
+                            Cells[item.X, item.Y + 1].Value == TypeCell.ShipBody ||
+                            Cells[item.X + 1, item.Y - 1].Value == TypeCell.ShipBody || Cells[item.X + 1, item.Y].Value == TypeCell.ShipBody || Cells[item.X + 1, item.Y + 1].Value == TypeCell.ShipBody)
+                            {
+                                return false;
+                            }
+                            else
+                            {
+                                if (countOperation == ship.Points.Count)
+                                    return true;
+                            }
+
+                        }
+                        break;
+                }
+            }
+
+            return true;
+        }
 
         public Ship CreateShip(int x, int y, ShipDirection direction, TypeShips type)
         {
@@ -216,12 +398,13 @@ namespace SeaBattle.Model
 
             return newShip;
         }
-
-        public bool AnyShipsAround(Point point) 
+        public bool AnyShipsAroundPoint(Point point) 
         {
-            return  Cells[point.X - 1, point.Y - 1].Value == TypeCell.ShipBody && Cells[point.X - 1, point.Y].Value == TypeCell.ShipBody && Cells[point.X - 1, point.Y + 1].Value == TypeCell.ShipBody &&
+            return Cells[point.X - 1, point.Y - 1].Value == TypeCell.ShipBody && Cells[point.X - 1, point.Y].Value == TypeCell.ShipBody &&  Cells[point.X - 1, point.Y + 1].Value == TypeCell.ShipBody &&
                     Cells[point.X, point.Y - 1].Value == TypeCell.ShipBody && Cells[point.X, point.Y + 1].Value == TypeCell.ShipBody &&
                     Cells[point.X + 1, point.Y - 1].Value == TypeCell.ShipBody && Cells[point.X + 1, point.Y].Value == TypeCell.ShipBody && Cells[point.X + 1, point.Y + 1].Value == TypeCell.ShipBody;
         }
+
+
     }
 }
